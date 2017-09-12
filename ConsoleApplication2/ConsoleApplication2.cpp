534a1cd2f6c3f509034e10114ae89420e4892a81
wilkinsonbr <wilkinsonbr@SOB-B2113-18>
2017-09-12 18:43:52 -0400

    In Class Work

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
+#include "stdio.h"
+using namespace std;
 
 
 int main()
 {
+	char students[25][100];
+	int count=0;
+	bool name = true;
+	
+	
+	
+
+START: while (name && count < 24)
+	{
+		cout << "Enter students " << count << " name" << endl;
+		
+		gets_s(students[count++]);
+		
+		
+			cout << "Would you like to add another student?" << endl;
+			cout << "Type 1 for Yes, Type 0 for No" << endl;
+			cin >> name;
+			cin.ignore();
+			cin.clear();
+			
+		
+		
+	}
+	
+		   cout << "You have " << count << " Students" << endl;
+
     return 0;
 }
 
